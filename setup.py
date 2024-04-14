cf21affa7c84ee488d26fba3dfbfc54a4344f673
Visagan Ravindran <9424825-Inker2401@users.noreply.gitlab.com>
2024-04-14 18:57:38 +0100

    Added requirement for Python 3.6
    f-strings, introduced in Python 3.6 are used at some points in the code instead of the format attribute.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import find_packages
-from setuptools import setup,Extension
+from setuptools import setup, Extension
 
 setup(name="CASTEPbands",
       version="1.3.0",
@@ -9,10 +9,8 @@ setup(name="CASTEPbands",
       author="Zachary Hawkhead, Nathan Philip Bentley, Visagan Ravindran",
       author_email="zachary.hawkhead@ymail.com",
       license="MIT",
+      python_requires='>=3.6',
       install_requires=["numpy",
                         "matplotlib",
                         "ase>=3.18.1"],
       )
-
-
-
