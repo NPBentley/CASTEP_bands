d3708a3e413486c8480d82a4f112f0fc273059ba
Visagan Ravindran <9424825-Inker2401@users.noreply.gitlab.com>
2024-04-15 10:56:40 +0100

    Applied autopep8

--- CASTEPbands/Spectral.py
+++ CASTEPbands/Spectral.py
@@ -1199,7 +1199,7 @@ class Spectral:
             eng_label = r'E-E$_{\mathrm{VBM}}$'
         elif self.zero_cbm is True:
             eng_label = r'E-E$_{\mathrm{CBM}}$'
-        ax.set_ylabel(eng_label + f' ({eng_unit})', fontsize=fontsize)
+        ax.set_ylabel(eng_label + f' ({eng_unit})', fontsize = fontsize)
 
         ax.set_xlim(1, len(self.kpoints))
         ax.tick_params(axis='both', direction='in', which='major', labelsize=fontsize * 0.8, length=12, width=1.2)
@@ -1723,7 +1723,7 @@ class Spectral:
                 eng_label = r'E-E$_{\mathrm{VBM}}$'
             elif self.zero_cbm is True:
                 eng_label = r'E-E$_{\mathrm{CBM}}$'
-            ax.set_ylabel(eng_label + f' ({eng_unit})', fontsize=fontsize)
+            ax.set_ylabel(eng_label + f' ({eng_unit})', fontsize = fontsize)
 
             ax.tick_params(axis='both', which='major', labelsize=fontsize * 0.8, length=12, width=1.2)
             ax.tick_params(axis='both', which='minor', labelsize=fontsize * 0.8, length=6,
