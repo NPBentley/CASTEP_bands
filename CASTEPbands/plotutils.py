c48284e4ff521744547918278cf67a50f038d65a
Visagan Ravindran <9424825-Inker2401@users.noreply.gitlab.com>
2024-05-03 12:37:20 +0100

    Added function to plot only valence and conduction band (primarily for convenience purposes)
    Other misc_changes
    * Renamed spin_ch arguments in plotutils.py to spin_index for consistency with the main Spectral.py module
    * Added linewidth and linestyle arguments to add_bands

--- CASTEPbands/plotutils.py
+++ CASTEPbands/plotutils.py
@@ -2,18 +2,18 @@
 
 This allows for the creation of more complicated plots.
 In general, the axes must already be created as a pre-requisitie for these functions.
-
-Created by: V Ravindran , 22/04/2024
 """
-import CASTEPbands.Spectral as Spectral
+# Created by: V. Ravindran, 22/04/2024
 import matplotlib as mpl
 import numpy as np
+
+from CASTEPbands import Spectral
 
 
 def plot_bands(spec: Spectral.Spectral, ax: mpl.axes._axes.Axes, band_ids: np.ndarray,
                color: 'str | list' = None, label_first: str = None, labels: list = None,
-               linewidth: int = 1.2, linestyle: str = '-',
-               spin_ch: int = None):
+               linewidth: float = 1.2, linestyle: str = '-',
+               spin_index: int = None):
     """Add a specified set of bands to an existing bandstructure axis.
 
     By default, bands for both spin channels will be done if spin polarised but only the first spin
@@ -35,7 +35,11 @@ def plot_bands(spec: Spectral.Spectral, ax: mpl.axes._axes.Axes, band_ids: np.nd
         the label for the first band in the set (default : none)
     labels : list
         labels to use for each band (default : none)
-    spin_ch : int
+    linewidth : float
+        width of lines for bands (default = 1.2)
+    linestyle : str
+        style of lines for bands (default = '-')
+    spin_index : int
         spin channel to plot (default : none)
 
     Raises
@@ -71,6 +75,7 @@ def plot_bands(spec: Spectral.Spectral, ax: mpl.axes._axes.Axes, band_ids: np.nd
             if do_label_first is True and i == 0:
                 # Label the first band in the set using a separate label if requested
                 ax.plot(kpts, banddata[nb, :, ns], color=color,
+                        linestyle=linestyle, linewidth=linewidth,
                         label=label_first)
                 # Make sure to set the flag to false, otherwise, otherwise all bands
                 # will be labelled - not what we want!
@@ -79,14 +84,15 @@ def plot_bands(spec: Spectral.Spectral, ax: mpl.axes._axes.Axes, band_ids: np.nd
             elif do_labels is True:
                 # Label bands
                 ax.plot(kpts, banddata[nb, :, ns], color=color,
+                        linestyle=linestyle, linewidth=linewidth,
                         label=labels[i])
             else:
                 # Just plot the bands
                 ax.plot(kpts, banddata[nb, :, ns], color=color)
 
     # Decide on spins to do. By default, do both spins
-    if spin_ch is not None:
-        _add_bands_for_spin(spin_ch)
+    if spin_index is not None:
+        _add_bands_for_spin(spin_index)
     else:
         # Do both spin channels, we will only add labels for the first spin channel.
         add_labels = True
@@ -94,11 +100,80 @@ def plot_bands(spec: Spectral.Spectral, ax: mpl.axes._axes.Axes, band_ids: np.nd
             if ns == 1:
                 add_labels = False
             _add_bands_for_spin(ns, add_labels=add_labels)
-    return
 
 
-def color_vb_cb():
-    raise NotImplementedError
+def add_vb_cb(spec: Spectral.Spectral, ax: mpl.axes._axes.Axes,
+              colors: list = ['b', 'r'], labels: list = None,
+              linewidth: float = 1.2, linestyle: str = '-',
+              spin_index: int = None):
+    """Plot only the valence and conduction bands on the plot
+
+    By default, bands for both spin channels will be done if spin polarised but only the first spin
+    channel will have labels added.
+
+    Parameters
+    ----------
+    spec : Spectral.Spectral
+        the bands data read from CASTEP
+    ax : mpl.axes._axes.Axes
+        the axes to plot the bands on
+    colors : list
+        colour to use for valence band and conduction band (for both spin channels if spin_index is None)
+    labels : list
+        labels to use for valence band and conduction band
+    linewidth : float
+        width of lines for bands (default = 1.2)
+    linestyle : str
+        style of lines for bands (default = '-')
+    spin_index : int
+        spin channel to plot (default : none)
+
+    Raises
+    ------
+    IndexError
+        colour/labels do not have the same length as band_ids
+    """
+    if labels is not None:
+        labels = list(labels)
+        if len(labels) != 2:
+            raise IndexError('Labels must specified for both VBM and CBM')
+
+    # Get the number of electrons in each spin channel
+    if spec.nspins == 1:
+        nelec = np.array([spec.electrons], dtype=int)
+    else:
+        nelec = np.array([spec.no_up, spec.no_down], dtype=int)
+
+    def _get_vb_cb(ns: int):
+        """Get valence and conduction bands for each spin channel."""
+        vb_eigs = spec.BandStructure[int(nelec[ns] / spec.occ) - 1, :, ns]
+        cb_eigs = spec.BandStructure[int(nelec[ns] / spec.occ), :, ns]
+        return vb_eigs, cb_eigs
+
+    # Decide which valence and conduction band we are going to plot
+    if spin_index is not None:
+        # Plot only the VB and CB for the given spin channel
+        vb_eigs, cb_eigs = _get_vb_cb(spin_index)
+        ax.plot(spec.kpoints, vb_eigs, color=colors[0],
+                linestyle=linestyle, linewidth=linewidth,
+                label=labels[0])
+        ax.plot(spec.kpoints, cb_eigs, color=colors[1],
+                linestyle=linestyle, linewidth=linewidth,
+                label=labels[1])
+    else:
+        # Plot both spin channels - label only the first spin
+        for ns in range(spec.nspins):
+            if ns == 1:
+                # Do not want to add labels for second spin channel
+                labels = [None, None]
+
+            vb_eigs, cb_eigs = _get_vb_cb(ns)
+            ax.plot(spec.kpoints, vb_eigs, color=colors[0],
+                    linestyle=linestyle, linewidth=linewidth,
+                    label=labels[0])
+            ax.plot(spec.kpoints, cb_eigs, color=colors[1],
+                    linestyle=linestyle, linewidth=linewidth,
+                    label=labels[1])
 
 
 def compare_bands():
