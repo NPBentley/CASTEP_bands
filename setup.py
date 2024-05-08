592faca9c280870e9127a2bb650ec80025dc0938
Visagan Ravindran <157363044+Inker2401@users.noreply.github.com>
2024-05-08 16:01:21 +0100

    Updated version in setup.py and CITATION.
    * Added setup.cfg for pre-commit hooks with Flake and other linters
    
    * Updated CITATION and setup.py with new versions
    
    * Added requirement for Python 3.6 as f-strings, are used at some points in the code instead of the format attribute.
    
    ---------
    
    Authored-by: Visagan Ravindran <9424825-Inker2401@users.noreply.gitlab.com>

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 from setuptools import find_packages
-from setuptools import setup,Extension
+from setuptools import setup, Extension
 
 setup(name="CASTEPbands",
-      version="0.1.0",
+      version="1.3.0",
       description="CASTEP module for plotting band structures and phonon dispersions.",
       packages=find_packages(),
       url="https://github.com/NPBentley/CASTEP_bands.git",
       author="Zachary Hawkhead, Nathan Philip Bentley, Visagan Ravindran",
       author_email="zachary.hawkhead@ymail.com",
       license="MIT",
+      python_requires='>=3.6',
       install_requires=["numpy",
                         "matplotlib",
                         "ase>=3.18.1"],
       )
-
-
-
