a07f949cde87fad5ef038077dc6c46305d015359
Visagan Ravindran <9424825-Inker2401@users.noreply.gitlab.com>
2024-11-13 16:25:03 +0000

    Added phonon documentation and basic plotting code - more options to be added later

--- CASTEPbands/phonon.py
+++ CASTEPbands/phonon.py
@@ -12,6 +12,9 @@ import numpy as np
 
 from ase import spacegroup as spg
 
+from CASTEPbands import spgutils
+from matplotlib import ticker
+
 
 def _read_phonon_freqs(phononfile: str):
     """Read the phonon frequencies and wavevectors (qpoints) from a .phonon file.
@@ -26,7 +29,7 @@ def _read_phonon_freqs(phononfile: str):
 
     Returns
     -------
-    qpt_list : ndarray shape=(nqpts, 3)
+    qpt_array : ndarray shape=(nqpts, 3)
         list of phonon wavevectors (qpoints)
     qpt_weights : ndarray shape=(nqpoints)
         weights for each phonon wavevector
@@ -149,7 +152,8 @@ def _read_phonon_freqs(phononfile: str):
         # in one go, we use a combination of status flags to determine what we are reading.
         phonon_freqs = np.empty((nbranch, nqpts), dtype=float)
         qpt_weights = np.empty(nqpts, dtype=float)
-        qpt_list = np.empty((nqpts, 3), dtype=float)
+        qpt_array = np.empty((nqpts, 3), dtype=float)  # array of wavevectors
+        qpt_list = []  # list of wavevector indices
 
         # QUESTION Do we want to read phonon eigenvectors?
         have_eigenmodes, have_qpt = False, False
@@ -160,7 +164,8 @@ def _read_phonon_freqs(phononfile: str):
                 # We have a q-point so read it
                 iq, qx, qy, qz, qw = line.split()[1:]
                 iq = int(iq)-1  # CASTEP uses Fortran ordering which starts from 1
-                qpt_list[iq] = np.array([qx, qy, qz], dtype=float)
+                qpt_list.append(iq)
+                qpt_array[iq] = np.array([qx, qy, qz], dtype=float)
                 qpt_weights[iq] = float(qw)
                 read_qpts += 1
                 have_qpt = True
@@ -192,23 +197,77 @@ def _read_phonon_freqs(phononfile: str):
                 continue
 
     # Finished reading the file - check we actually have all of it
+    qpt_list = np.array(qpt_list, dtype=int)
     if read_qpts != nqpts:
-        raise AssertionError(f'Expected {nqpts} wavevectors in file but only have {read_qpts}')
+        raise AssertionError(f'Expected {nqpts} wavevectors in file but only have read {read_qpts}')
+    if qpt_list.shape[0] != nqpts:
+        raise AssertionError(f'Expected {nqpts} wavevectors in file but only have {read_qpts} qpoint indices')
 
-    return qpt_list, qpt_weights, phonon_freqs, cell_vecs, species_list, frac_coords
+    # Sort phonon wavevectors in order - this should not be necessary but let's be safe
+    sort_indx = qpt_list.argsort()
+    qpt_list = qpt_list[sort_indx]
+    qpt_array = qpt_array[sort_indx]
+    qpt_weights = qpt_weights[sort_indx]
+    for ibranch in range(nbranch):
+        phonon_freqs[ibranch, :] = phonon_freqs[ibranch, sort_indx]
+
+    return qpt_array, qpt_weights, phonon_freqs, cell_vecs, species_list, frac_coords
 
 
 class Phonon:
-    # TODO Documentation
+    """
+    The phonon information from a CASTEP .phonon file.
+
+    The actual data is stored in the array freqs with the following shape
+    freqs[branches/modes, q-points]
+    where q-points refers to the k-points at which the perturbation is computed.
+
+    Attributes
+    ----------
+    nqpoint : int
+        number of q-points in phonon calculation
+    nbranch : int
+        number of branches/modes in the phonon calculation
+    qpoints : ndarray
+        q-points for phonon calculation in fractional coordinates
+    qpoint_weights : ndarray
+        weights for each q-point
+    cell : ase.atoms.Atoms
+        unit cell for CASTEP calculation
+    high_sym : ndarray
+        qpoint index of high-symmetry points
+    high_sym : list
+        labels for each high-symmetry points
+
+    Methods
+    -------
+    plot_dispersion
+        plot the phonon dispersion or band structure
+    """
 
     def __init__(self,
-                 seedname: str,
-                 verbose: bool = False
+                 phononfile: str,
+                 verbose: bool = False,
+                 high_sym_spacegroup: bool = True,
+                 override_bv: str = None,
                  ):
-        # TODO Documentation
+        """Initialises the phonon data.
+
+        Parameters
+        ----------
+        seedname : str
+            CASTEP seedname
+        verbose : bool
+            enable high output verbosity
+        high_sym_spacegroup : bool
+            uses high symmetry labels based on Bravais lattice
+            for spacegroup (CASTEP Default)
+        override_bv : str
+            override the high-symmetry labels and use specified
+            Bravais lattice instead for labels
+        """
 
         # Open the phonon output file
-        phononfile = seedname + '.phonon'
         try:
             open(phononfile, 'r', encoding='ascii')
         except FileNotFoundError as exc:
@@ -230,7 +289,7 @@ class Phonon:
             print('Number of ions        ', len(species_list))
             print('Number of species     ', len(set(species_list)))
 
-        # DEBUG - Check same as written by CASTEP
+        # DEBUG - Check q-points and frequencies same as written by CASTEP
         # for iq in range(self.nqpoint):
         #     print('q-pt= {:5n} {:10.6f} {:10.6f} {:10.6f} {:14.10f}'.format(iq+1, *self.qpoints[iq, :], self.qpoint_weights[iq]))
         #     for ibranch in range(self.nbranch):
@@ -249,3 +308,88 @@ class Phonon:
             # Get spacegroup of cell
             spg_cell = spg.get_spacegroup(self.cell)
             print(f'Space group: {spg_cell.no}', "".join(spg_cell.symbol.split()))
+
+        # Finally, create the high-symmetry lines
+        self.high_sym, self.high_sym_labels = spgutils._get_high_sym_lines(self.qpoints, self.cell,
+                                                                           high_sym_spacegroup=high_sym_spacegroup,
+                                                                           override_bv=override_bv
+                                                                           )
+
+    def plot_branch(self, ax,
+                    linestyle='-',
+                    linewidth=1.2
+                    ):
+        # TODO Choose which branches/mode to plot
+        # Loop over all modes and plot them
+        qpt_indx = np.arange(self.nqpoint)
+        for ibranch in range(self.nbranch):
+            ax.plot(qpt_indx, self.freqs[ibranch])
+
+    def plot_dispersion(self, ax: mpl.axes.Axes,
+                        axes_only: bool = False,
+                        linestyle: str = '-',
+                        linewidth: float = 1.2,
+                        freq_lim: list = None,
+                        klim: list = None,
+                        sym_lines: bool = True,
+                        fontsize: float = 20.0
+                        ):
+        """Plot the phonon dispersion/band structure.
+
+        Parameters
+        ----------
+        ax : mpl.axes.Axes
+            axes to plot phonon data
+        axes_only : do not plot data, initialise high symmetry lines and axes labels only
+            do not plot data, initialise high symmetry lines and axes labels only
+        linestyle : str
+            type of line to use when plotting phonon frequencies
+        linewidth : float
+            thickness of line for phonon frequencies
+        freq_lim : ndarray
+            limits of frequency axis (in cm^-1)
+        klim : list(dtype=str) or list(dtype=float) - see below
+            Limit of k-point axes.
+            This can be specified in by either the high-symmetry point labels (string) or kpoint index (integer)
+        sym_lines : bool
+            display horizontal lines indicating high-symmetry points
+        fontsize : float
+            font size to use in plot
+        """
+
+        # TODO Choose which branches/mode to plot
+        # TODO Set k-point limits
+
+        # Set axis labels and tick intervals and all that boring stuff
+        eng_unit = r'$\text{cm}^{-1}$'
+        ax.set_ylabel(r'$\omega$ ' + f'({eng_unit})', fontsize=fontsize)
+
+        ax.set_xlim(0, len(self.qpoints)-1)
+        ax.set_ylim(np.amin(self.freqs[0, :])-10, np.amax(self.freqs[-1, :]+10))
+        if klim is not None:
+            plot_klim = spgutils.get_klim(self.high_sym, self.high_sym_labels, klim)
+            ax.set_xlim(plot_klim[0], plot_klim[1])
+        if freq_lim is not None:
+            ax.set_ylim(freq_lim[0], freq_lim[1])
+
+        ax.tick_params(axis='both', direction='in', which='major', labelsize=fontsize * 0.8, length=12, width=1.2)
+        ax.tick_params(axis='both', which='minor', labelsize=fontsize * 0.8, length=6,
+                       right=True, top=False, bottom=False, left=True, width=1.2)
+
+        ax.set_xticks(self.high_sym)
+        ax.set_xticklabels(self.high_sym_labels)
+        ax.minorticks_on()
+        ax.xaxis.set_minor_locator(ticker.AutoMinorLocator(2))
+        ax.yaxis.set_minor_locator(ticker.AutoMinorLocator(2))
+
+        # Draw high-symmetry lines
+        if sym_lines:
+            for i in self.high_sym:
+                ax.axvline(i, color='k', linewidth=1)
+
+        # We're now in business - decide to stop here or continue
+        if axes_only:
+            return
+
+        # Plot phonon dispersion
+        self.plot_branch(ax, linestyle=linestyle, linewidth=linewidth)
